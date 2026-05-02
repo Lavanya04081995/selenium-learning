3389c86ffc568ce1a059facc9dee041c5f6fd26a
Lavanya04081995 <lavanya04.08.1995@gmail.com>
2026-05-02 15:35:21 +0530

    Add files via upload
    updated fibonacci series with reference .

---
+++ FibonacciSeries.java
@@ -0,0 +1,22 @@
+package week1.day1;
+import java.util.Scanner;
+
+public class FibonacciSeries{
+
+    public static void main(String[] args) {
+        int num;
+        int a=0 , b=1;
+        Scanner x =new Scanner(System.in);
+        System.out.println("Enter the range");
+        num =x.nextInt();
+        System.out.println(a+" "+b+" ");
+        int nextterm;
+        for(int i=2;i<num;i++){
+            nextterm=a+b;
+        a=b;
+        b=nextterm;
+        System.out.println(nextterm+" ");
+    }
+ }
+}
+
