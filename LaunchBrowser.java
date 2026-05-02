e1486a48f510e31f26f8862c5710059c25427769
Lavanya04081995 <lavanya04.08.1995@gmail.com>
2026-05-02 14:55:11 +0530

    Add files via upload
    Updated launch browser program.

---
+++ LaunchBrowser.java
@@ -0,0 +1,12 @@
+package week1.day1;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchBrowser {
+public static void main(String[] args) {
+    ChromeDriver driver=new ChromeDriver();
+    driver.manage().window().maximize();
+    driver.get("https://www.amazon.in/");
+    
+}
+}
