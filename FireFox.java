b230351ee37b97e8ca06efcdb6705351e2e2f567
Lavanya04081995 <lavanya04.08.1995@gmail.com>
2026-05-02 16:35:12 +0530

    Add files via upload
    Updated FireFox browser program.

---
+++ FireFox.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class FireFox {
+public static void main(String[] args) {
+    float browserVersion = 100.2f;
+    String browser = "FireFox";
+    boolean isVisible = true;
+    int releaseYear = 2004;
+    char browserLogo = 'f';
+    System.out.println(browserVersion + " " + browser + " " + isVisible + " " + releaseYear + " " + browserLogo);
+
+}
+
+}
